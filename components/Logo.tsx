6f5d3dbb3498b26e0c78d9b555e564bf597681f9
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Pro.local>
2021-04-29 18:11:30 +0200

    Implemented the logos via a div.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,11 +7,13 @@ interface Props {
 
 export default function Logo({ light = true }: Props) {
     const renderImage = () => {
-        if (light) {
-            return <Image height={74} src='/assets/logo-light.jpg' width={345} />
-        } else {
-            return <Image height={74} src='/assets/logo-dark.jpg' width={345} />
-        }
+        return (
+            <div style={{
+                height: '70px',
+                backgroundImage: light ? "url('/assets/logo-light.jpg')" : "url('/assets/logo-dark.jpg')",
+                width: '342px'
+            }}></div>
+        )
     }
 
     return (
